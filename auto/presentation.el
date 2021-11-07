cb13c601e72729daa2c35c1832ced07bc807a215
Shane Mulligan <mullikine@gmail.com>
2021-11-07 16:24:02 +1300

    07.11.21

--- auto/presentation.el
+++ auto/presentation.el
@@ -6,13 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8x") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -32,16 +32,16 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:org22533c7"
-    "sec:orge720583"
-    "sec:org8def434"
-    "sec:org24d3f9f"
-    "sec:org26e9159"
-    "sec:org6bfd291"
-    "sec:orgb8c214e"
-    "sec:orgd4f946b"
-    "sec:org25a70e8"
-    "sec:orgeca91b5"
-    "sec:org7a1299e"))
+    "sec:org4473ba3"
+    "sec:org132b89c"
+    "sec:orge3eddba"
+    "sec:org127cb84"
+    "sec:org4bd376c"
+    "sec:orgbad59c3"
+    "sec:org82fad0a"
+    "sec:org32e640a"
+    "sec:orgbdc2e50"
+    "sec:orgb631e38"
+    "sec:org06e3e08"))
  :latex)
 
