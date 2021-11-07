0d58cff08cc932418cbb9a7d04a355602049c9ba
Shane Mulligan <mullikine@gmail.com>
2021-11-07 16:46:56 +1300

    Sun 07 Nov 2021 16:46:56

--- auto/presentation.el
+++ auto/presentation.el
@@ -6,13 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8x") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -32,18 +32,18 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:org721e568"
-    "sec:org0f01792"
-    "sec:org6a1aa4d"
-    "sec:org783de44"
-    "sec:orgfb6a8a7"
-    "sec:orgad8fa18"
-    "sec:org24ef7c1"
-    "sec:orga25339b"
-    "sec:orgc78236e"
-    "sec:org261646b"
-    "sec:orgb8e0b0e"
-    "sec:orgfcc0f5c"
-    "sec:orgf246133"))
+    "sec:orgf7f8806"
+    "sec:orgb57ca0c"
+    "sec:org71d6863"
+    "sec:org241dff6"
+    "sec:org6d4f411"
+    "sec:org8825e6d"
+    "sec:org359aa1f"
+    "sec:org38a29aa"
+    "sec:org0b4163c"
+    "sec:org47ad2e8"
+    "sec:orgae4fb25"
+    "sec:orgb3b03ea"
+    "sec:org4a4edee"))
  :latex)
 
