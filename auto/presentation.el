6be17f2a8b2a25c40e26235a47a7b482c5f5af8e
Shane Mulligan <mullikine@gmail.com>
2021-11-07 16:59:44 +1300

    07.11.21

--- auto/presentation.el
+++ auto/presentation.el
@@ -6,13 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8x") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -32,18 +32,18 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:org5d67a81"
-    "sec:org1e6148d"
-    "sec:orga905c47"
-    "sec:org01e446a"
-    "sec:org1930beb"
-    "sec:org7243c04"
-    "sec:org383f8e9"
-    "sec:org74eb43d"
-    "sec:org03c5ff2"
-    "sec:org9896608"
-    "sec:orgae71608"
-    "sec:orge9c6157"
-    "sec:org2fad53d"))
+    "sec:org2bca986"
+    "sec:org35bfbd1"
+    "sec:org64551f0"
+    "sec:org9071f0b"
+    "sec:org4dfb363"
+    "sec:orga04e0e1"
+    "sec:orgcc1ba0a"
+    "sec:org1e73ee5"
+    "sec:org714bc92"
+    "sec:org1508010"
+    "sec:org60674b8"
+    "sec:orgcbe8aa0"
+    "sec:orgd8cf648"))
  :latex)
 
