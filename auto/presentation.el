61199307a00d116d3712ee66b1d64ebd19aea9d5
Shane Mulligan <mullikine@gmail.com>
2021-11-07 16:02:28 +1300

    Sun 07 Nov 2021 16:02:28

--- auto/presentation.el
+++ auto/presentation.el
@@ -6,13 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8x") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -32,25 +32,25 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:org93e8174"
-    "sec:org8302ef6"
-    "sec:org4068df6"
-    "sec:org911c31f"
-    "sec:org4290d4e"
-    "sec:org68fd600"
-    "sec:orgb6d4b83"
-    "sec:orgfa85b4c"
-    "sec:org0a612bf"
-    "sec:org723041c"
-    "sec:orge2eb204"
-    "sec:org1a7ffdf"
-    "sec:org00e6169"
-    "sec:org4a6058c"
-    "sec:org2f62d2d"
-    "sec:orge2de227"
-    "sec:org56b6d68"
-    "sec:orga5917a6"
-    "sec:org5f8ffef"
-    "sec:orgdd525f7"))
+    "sec:org75a3d57"
+    "sec:orgd75ca5b"
+    "sec:org7036931"
+    "sec:org5fa3313"
+    "sec:orgabef21e"
+    "sec:org720a6ad"
+    "sec:orgf768638"
+    "sec:org5f567b7"
+    "sec:orgdd3f7ed"
+    "sec:orgb18c369"
+    "sec:orgf1a2b6b"
+    "sec:orge4c22bc"
+    "sec:org9ad0fda"
+    "sec:orge264443"
+    "sec:org8c26bbf"
+    "sec:org15394b5"
+    "sec:orgce9e938"
+    "sec:org8553d57"
+    "sec:org2d3a3b7"
+    "sec:org218174a"))
  :latex)
 
