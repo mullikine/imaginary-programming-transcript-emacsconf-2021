891542b8619a42da19b48303b98f5acad00bcc8d
Shane Mulligan <mullikine@gmail.com>
2021-11-07 13:59:20 +1300

    Sun 07 Nov 2021 13:59:20

---
+++ auto/presentation.el
@@ -0,0 +1,50 @@
+(TeX-add-style-hook
+ "presentation"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "presentation")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8x") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org8f066b8"
+    "sec:org1ef2c81"
+    "sec:org4a332f5"
+    "sec:org7b7b70b"
+    "sec:org9b4f8c8"
+    "sec:org791c6e4"
+    "sec:orgbdb1c02"
+    "sec:orgfe91fb7"
+    "sec:org261a709"
+    "sec:org0478860"
+    "sec:org6a23be1"
+    "sec:orgeb58518"
+    "sec:orgcdb097d"
+    "sec:org6f828c9"))
+ :latex)
+
